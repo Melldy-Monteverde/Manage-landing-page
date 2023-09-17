2c5a9f758e5fd5700127f848cd276c3c61597dc9
Lexther Monteverde <monteverdelex@gmail.com>
2023-09-17 18:54:52 -0300

    add structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,7 @@ module.exports = {
       colors: {
         'bright-red': '#F25F3A',
         'dark-blue': '#242D52',
-        'dark-grayish Blue': '#9095A7',
+        'dark-grayish-blue': '#9095A7',
         'very-dark-blue': '#1D1E25',
         'very-pale-red': '#FFEFEB',
         'vary-light-gray': '#FAFAFA'
